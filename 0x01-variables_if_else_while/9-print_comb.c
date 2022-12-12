b5ecbf268bf21f5c07e1bb660bee5ac19c8d7696
StanleyEmegbusim <stanley.emegbusim@gmail.comgit config --global user.name StanleyEmegbusim>
2022-12-12 07:03:13 -0800

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
-
 /**
- * main - print all combination of single digits
- * Return: Always 0
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	int n;
+	int a;
 
-	for (n = 48; n < 58; n++)
+	for (a = 0; a < 10; a++)
 	{
-		putchar(n);
-		if (n != 57)
+		putchar(a + '0');
+		if (a < 9)
 		{
 			putchar(',');
 			putchar(' ');
 		}
+	}
 	putchar('\n');
 	return (0);
 }
