076e29d03ed4924dc3688dce1adb69292f58280a
StanleyEmegbusim <stanley.emegbusim@gmail.comgit config --global user.name StanleyEmegbusim>
2022-12-14 04:47:37 -0800

    Nature made the natural numbers; All else is the work of women

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,8 +1,9 @@
 #include "main.h"
+
 /**
  * print_times_table - print times table
- * @n: times table to use
- * Return: void
+ * @n : times table to use
+ * Return:void
  */
 
 void print_times_table(int n)
@@ -10,6 +11,7 @@ void print_times_table(int n)
 	int a = 0, rep, b;
 	if (n < 0 || n > 15)
 		return;
+
 	while (a <= n)
 	{
 		for (b = 0; b <= n; b++)
@@ -42,6 +44,6 @@ void print_times_table(int n)
 			}
 		}
 		_putchar('\n');
-		return (0);
+		a++;
 	}
 }
--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,8 +2,9 @@
 #include <stdlib.h>
 /**
  * main - main block
- *
- * Return: 0.
+ * Description: computes and prints the sum of all the multiples of 3 or
+ * 5 below 1024 (excluded), followed by a new line
+ * Return: 0
  */
 int main(void)
 {
