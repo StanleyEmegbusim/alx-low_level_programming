26be3afc5b43b43e1d677112e6c6b07d2660f6d9
StanleyEmegbusim <stanley.emegbusim@gmail.comgit config --global user.name StanleyEmegbusim>
2022-12-15 14:03:36 -0800

    The prime factors

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,13 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
+
 /**
  * main - print the largest prime number
  *
  * Return: Always 0
  */
+
 int main(void)
 {
 	long prime = 612852475143, div;
+
 	while (div < (prime / 2))
 	{
 		if ((prime % 2) == 0)
@@ -15,6 +18,7 @@ int main(void)
 			prime /= 2;
 			continue;
 		}
+
 		for (div = 3; div < (prime / 2); div += 2)
 		{
 			if ((prime % div) == 0)
