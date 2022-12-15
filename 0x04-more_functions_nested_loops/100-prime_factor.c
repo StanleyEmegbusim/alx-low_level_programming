ec70f952b5868e24cd6665ecd0d58a92947f7034
StanleyEmegbusim <stanley.emegbusim@gmail.comgit config --global user.name StanleyEmegbusim>
2022-12-15 13:54:31 -0800

    Function that prints an integer

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,24 +1,26 @@
+#include <stdlib.h>
 #include <stdio.h>
-
 /**
- * main - prints largest prime factor.
- * Return: Always 0.
+ * main - print the largest prime number
+ *
+ * Return: Always 0
  */
-
 int main(void)
 {
-	long int n, fp;
-
-	n = 612852475143;
-	for (fp = 2; fp <= n; fp++)
+	long prime = 612852475143, div;
+	while (div < (prime / 2))
 	{
-		if (n % fp == 0)
+		if ((prime % 2) == 0)
 		{
-			n /= fp;
-			fp--;
+			prime /= 2;
+			continue;
 		}
-
+		for (div = 3; div < (prime / 2); div += 2)
+		{
+			if ((prime % div) == 0)
+				prime /= div;
+		}
 	}
-	printf("%1d\n", fp);
+	printf("%1d\n", prime);
 	return (0);
 }
