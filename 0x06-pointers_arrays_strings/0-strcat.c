210f05af5f9aaa842eb37260be71fb1f781c977e
StanleyEmegbusim <stanley.emegbusim@gmail.comgit config --global user.name StanleyEmegbusim>
2022-12-22 12:42:11 -0800

    Function that concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,12 +1,11 @@
 #include <string.h>
 #include "main.h"
 
-
 /**
- * _strcat - concatenates two strings
- * @dest: a string pointer
- * @src: another string pointer
- * Return: a string pointer
+ * _strcat - concatenates two strings
+ * @dest: a string pointer
+ * @src: another string pointer
+ * Return: a string pointer
  */
 
 char *_strcat(char *dest, char *src)
